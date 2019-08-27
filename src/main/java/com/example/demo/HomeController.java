fc11d72a7cde3de6048ed0db37f7501353fe0e42
buckeyerichsen <buckey.erichsen@gmail.com>
2019-08-27 14:31:07 -0400

    sixth push

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @GetMapping("/car")
+    public String loadTvForm(Model model){
+        model.addAttribute("car", new Car());
+        return "car";
+    }
+
+    @PostMapping("/car")
+    public String processTvForm(@Valid Car car,
+        BindingResult result){
+        if (result.hasErrors()){
+            return "car";
+        }
+        return "carconfirm";
+    }
+}
